847e31c6f86d8554f835dd86937b8b4badc624e1
Aditya <229662497+adityade8k@users.noreply.github.com>
2025-12-11 08:27:35 -0500

    test

--- src/components/sceneCanvas/index.jsx
+++ src/components/sceneCanvas/index.jsx
@@ -312,7 +312,7 @@ function SceneRoot() {
   ]);
 
   const blockPosition = [0, -0.5, -0.5];
-  const blockRotation = [-Math.PI/2, 0, 0];
+  const blockRotation = [0, 0, 0];
   const blockScale = 1;
 
   return (
